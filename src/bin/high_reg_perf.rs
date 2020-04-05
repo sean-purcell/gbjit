daead0b4707711a8a6a1132174a34486fc58daaa
Sean Purcell <me@seanp.xyz>
2020-04-05 15:31:35 -0500

    Do some tests on the fastest way to set the carry flag

--- src/bin/high_reg_perf.rs
+++ src/bin/high_reg_perf.rs
@@ -16,14 +16,23 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let xchg_offset = create_bench(&mut ops, bench_xchg);
     let stack_offset = create_bench(&mut ops, bench_stack);
+    let stc_offset = create_bench(&mut ops, bench_stc);
+    let add_offset = create_bench(&mut ops, bench_add);
+    let sahf_offset = create_bench(&mut ops, bench_sahf);
 
     let buf = ops.finalize().unwrap();
     let xchg: extern "sysv64" fn(u64) = unsafe { mem::transmute(buf.ptr(xchg_offset)) };
     let stack: extern "sysv64" fn(u64) = unsafe { mem::transmute(buf.ptr(stack_offset)) };
+    let stc: extern "sysv64" fn(u64) = unsafe { mem::transmute(buf.ptr(stc_offset)) };
+    let add: extern "sysv64" fn(u64) = unsafe { mem::transmute(buf.ptr(add_offset)) };
+    let sahf: extern "sysv64" fn(u64) = unsafe { mem::transmute(buf.ptr(sahf_offset)) };
 
     let iter = 100_000_000;
     println!("Xchg: {:#?}", time_execution(|| xchg(iter)));
     println!("Stack: {:#?}", time_execution(|| stack(iter)));
+    println!("Stc: {:#?}", time_execution(|| stc(iter)));
+    println!("Add: {:#?}", time_execution(|| add(iter)));
+    println!("Sahf: {:#?}", time_execution(|| sahf(iter)));
 
     Ok(())
 }
@@ -32,6 +41,7 @@ fn create_bench(ops: &mut x64::Assembler, f: impl FnOnce(&mut x64::Assembler)) -
     let offset = ops.offset();
     dynasm!(ops
         ; sub rsp, 8
+        ; mov BYTE [rsp + 2], 1
         ; start:
         ;; f(ops)
         ; sub rdi, 1
@@ -65,5 +75,39 @@ fn bench_stack(ops: &mut x64::Assembler) {
         ; mov ah, ch
         ; mov dh, ah
         ; mov ah, [rsp]
+    )
+}
+
+fn bench_stc(ops: &mut x64::Assembler) {
+    dynasm!(ops
+        ; test [rsp + 2], 1
+        ; jz >l1
+        ; stc
+        ; jmp >l2
+        ; l1:
+        ; clc
+        ; l2:
+        ; adc al, ah
+    )
+}
+
+fn bench_add(ops: &mut x64::Assembler) {
+    dynasm!(ops
+        ; mov [rsp], ah
+        ; mov ah, [rsp + 2]
+        ; and ah, 1
+        ; add ah, BYTE 255 as _
+        ; mov ah, [rsp]
+        ; adc al, ah
+    )
+}
+
+fn bench_sahf(ops: &mut x64::Assembler) {
+    dynasm!(ops
+        ; mov [rsp], ah
+        ; mov ah, [rsp + 2]
+        ; sahf
+        ; mov ah, [rsp]
+        ; adc al, ah
     )
 }
